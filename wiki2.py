ba5ce9c9d5310e52c3e0f0f12ab64b934af36ee5
miki <miki.y6417@gmail.com>
2017-09-28 08:22:00 +0900

    NewFileCommit

---
+++ wiki2.py
@@ -0,0 +1,10 @@
+import requests, sys
+import codecs
+search_word = sys.argv[1]
+api_url = 'https://ja.wikipedia.org/w/api.php'
+api_params = {'format':'xmlfm','action':'query','prop':'revisions','rvprop':'content'}
+api_params['titles'] = search_word
+wiki_data = requests.get(api_url, params=api_params)
+fo = codecs.open('C:\\Users\\miki\\Desktop'+ search_word + '.html', 'w', 'utf-8')
+fo.write(wiki_data.text)
+fo.close()
