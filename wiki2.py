4a6c30fbed698aadb34e26b341672d1af1ebdde9
miki-yoshida <miki.y6417@gmail.com>
2017-11-03 16:58:32 +0900

    Update wiki2.py

--- wiki2.py
+++ wiki2.py
@@ -1,10 +1,9 @@
-import requests, sys
-import codecs
+import requests, sys ,codecs
 search_word = sys.argv[1]
 api_url = 'https://ja.wikipedia.org/w/api.php'
 api_params = {'format':'xmlfm','action':'query','prop':'revisions','rvprop':'content'}
 api_params['titles'] = search_word
 wiki_data = requests.get(api_url, params=api_params)
-fo = codecs.open('C:\\Users\\miki\\Desktop'+ search_word + '.html', 'w', 'utf-8')
+fo = codecs.open('C:\\Users\\miki\\Desktop\\' + search_word + '.html', 'w', 'utf-8')
 fo.write(wiki_data.text)
 fo.close()
